5b56d81387ef9edd5fce288914da51c5164a82e7
lvntky <klevent1903@gmail.com>
2021-02-14 15:27:02 +0000

    notebook added

--- src/tsa.py
+++ src/tsa.py
@@ -1,8 +1,20 @@
 # import the libraries
-import re
-import nltk
+import re # regular expressions
+import nltk # text manuplations
+import warnings
 import string
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+#optional pandas and warning settings
+pd.set_option("display.max_colwidth", 200)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#load and read train and test data 
+train = pd.read_csv('../Data/train_data.csv')
+test = pd.read_csv('../Data/test_data.csv')
+
+# Data Inspection
+print(train[train['label'] == 0].head(10))
